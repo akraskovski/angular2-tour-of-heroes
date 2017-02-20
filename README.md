193641fb464f98408175e14372478538d401dd88
Artem Kraskovskij <tema.kras@mail.ru>
2017-02-20 14:11:04 +0300

    start structure created

---
+++ README.md
@@ -0,0 +1,1 @@
+#Angular2 Tour of Heroes tutorial.
---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>Angular 2 application</h1>
+    `
+})
+export class AppComponent {
+
+}
