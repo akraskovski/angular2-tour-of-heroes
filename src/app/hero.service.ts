7623b2c887f32fcd5f48d63c605d3372feaf096a
akraskovski <artem.kraskovski@gmail.com>
2017-05-27 18:42:15 +0300

    migrated to angular 4

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,10 +3,16 @@ import {Hero} from "./hero";
 import {HEROES} from "./mock-heroes";
 
 @Injectable()
-export class HeroService{
-
+export class HeroService {
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
+    }
+
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise(resolve => {
+            // Simulate server latency with 2 second delay
+            setTimeout(() => resolve(this.getHeroes()), 2000);
+        });
     }
 
     getHero(id: number): Promise<Hero> {
