1f647af559ce224061adb840556257e3699dca2d
akraskovski <artem.kraskovski@gmail.com>
2017-05-29 11:39:28 +0300

    fix project styles

--- src/systemjs-angular-loader.js
+++ src/systemjs-angular-loader.js
@@ -3,42 +3,47 @@ var stylesRegex = /styleUrls *:(\s*\[[^\]]*?\])/g;
 var stringRegex = /(['`"])((?:[^\\]\\\1|.)*?)\1/g;
 
 module.exports.translate = function(load){
-  if (load.source.indexOf('moduleId') != -1) return load;
+    if (load.source.indexOf('moduleId') != -1) return load;
 
-  var url = document.createElement('a');
-  url.href = load.address;
+    var url = document.createElement('a');
+    url.href = load.address;
 
-  var basePathParts = url.pathname.split('/');
+    var basePathParts = url.pathname.split('/');
 
-  basePathParts.pop();
-  var basePath = basePathParts.join('/');
+    basePathParts.pop();
+    var basePath = basePathParts.join('/');
 
-  var baseHref = document.createElement('a');
-  baseHref.href = this.baseURL;
+    var baseHref = document.createElement('a');
+    baseHref.href = this.baseURL;
+    baseHref = baseHref.pathname;
 
-  load.source = load.source
-    .replace(templateUrlRegex, function(match, quote, url){
-      var resolvedUrl = url;
+    if (!baseHref.startsWith('/base/')) { // it is not karma
+        basePath = basePath.replace(baseHref, '');
+    }
 
-      if (url.startsWith('.')) {
-        resolvedUrl = basePath + url.substr(1);
-      }
+    load.source = load.source
+        .replace(templateUrlRegex, function(match, quote, url){
+            var resolvedUrl = url;
 
-      return 'templateUrl: "' + resolvedUrl + '"';
-    })
-    .replace(stylesRegex, function(match, relativeUrls) {
-      var urls = [];
+            if (url.startsWith('.')) {
+                resolvedUrl = basePath + url.substr(1);
+            }
 
-      while ((match = stringRegex.exec(relativeUrls)) !== null) {
-        if (match[2].startsWith('.')) {
-          urls.push('"' + basePath + match[2].substr(1) + '"');
-        } else {
-          urls.push('"' + match[2] + '"');
-        }
-      }
+            return 'templateUrl: "' + resolvedUrl + '"';
+        })
+        .replace(stylesRegex, function(match, relativeUrls) {
+            var urls = [];
 
-      return "styleUrls: [" + urls.join(', ') + "]";
-    });
+            while ((match = stringRegex.exec(relativeUrls)) !== null) {
+                if (match[2].startsWith('.')) {
+                    urls.push('"' + basePath + match[2].substr(1) + '"');
+                } else {
+                    urls.push('"' + match[2] + '"');
+                }
+            }
 
-  return load;
+            return "styleUrls: [" + urls.join(', ') + "]";
+        });
+
+    return load;
 };
